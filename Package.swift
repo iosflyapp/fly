535909e2219518f6d7cf3750d7cd0bd22b025c8b
iosflyapp <taawdesign@gmail.com>
2025-12-27 11:27:23 -0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,17 +1,8 @@
 // swift-tools-version: 5.9
 import PackageDescription
-
 let package = Package(
     name: "SwiftIDE",
     platforms: [.iOS(.v16)],
-    products: [
-        .executable(name: "SwiftIDE", targets: ["SwiftIDE"])
-    ],
-    targets: [
-        .executableTarget(
-            name: "SwiftIDE",
-            path: ".",
-            sources: ["main.swift"]
-        )
-    ]
+    products: [.executable(name: "SwiftIDE", targets: ["SwiftIDE"])],
+    targets: [.executableTarget(name: "SwiftIDE", path: ".", sources: ["main.swift"])]
 )
